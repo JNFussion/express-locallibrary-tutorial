a2c555b13a7d868a1f3a5c0c1ba38e35c9501c70
JNFussion <juan.jose.gutierrez.99@gmail.com>
2022-03-05 12:05:34 +0100

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB =
+var dev_db_url =
   "mongodb+srv://JNFussion:Sjd5XBML98p8BWj@cluster0.ljzgq.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
